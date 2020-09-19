acce36624d1ccd86922eea5d0838d02b6758e070
Siddhi Rawle <siddhirawle45@gmail.com>
2020-09-19 20:38:29 +0530

    my birthday

---
+++ site/website/login3.js
@@ -0,0 +1,13 @@
+function validate()
+{
+	var username=document.getElementById("username").value;
+	var password=document.getElementById("password").value;
+		if(username == "admin" && password == "user")
+			{
+				window.open("FIRST.html",'newwindow');
+			}
+		else
+		{
+			alert("Error");
+		}
+}
